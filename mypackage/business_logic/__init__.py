16912588e4f496bb3763cfd0155baf3676c68f61
mike47967 <70372897+mike47967@users.noreply.github.com>
2023-10-14 13:48:44 +0300

    Update __init__.py

--- mypackage/business_logic/__init__.py
+++ mypackage/business_logic/__init__.py
@@ -66,4 +66,12 @@ def broadcast_starting_points(bot: TeleBot, messages: dict[int, str]) -> dict[in
     :return: Dictionary of results: {chat_id: result}
     """
     result = {}
+    
+    for i in messages.keys():
+        msgreturn = bot.send_message(i, messages[i])
+        if msgreturn.message_id:
+            result[i] = True
+        else:
+            result[i] = False
+    
     return result
